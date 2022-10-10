c20258a683617e63ae791115ad0e467106657fa7
dhaiduk <dementeyyy@gmail.com>
2022-10-10 09:13:44 +0300

    redux-thunk

--- clientmulti/src/redux/store.js
+++ clientmulti/src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers} from 'redux';
+import { createStore, combineReducers,applyMiddleware } from 'redux';
 import CountReducer from './reducers/countReducer';
+import ReduxThunk from 'redux-thunk';
  
 const rootReducer = combineReducers({
   count: CountReducer,
 });
  
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer,{}, applyMiddleware(ReduxThunk));
